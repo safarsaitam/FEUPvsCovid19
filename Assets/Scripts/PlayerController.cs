4b73d7fa932e9adb8347d2b64ef9d46db1911aae
unknown <jrffmatias@gmail.com>
2021-02-18 19:36:51 +0000

    Add player to game
    Added a player to the main scene
    Added movement to the player
    Added player movement animation
    Added camera which follows player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private Rigidbody2D rigidBody;
+    private Vector2 moveInput;
+    private Animator animator;
+    private bool playerMoving;
+    private Vector2 lastMove;
+
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        playerMoving = false;
+
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+
+        if (moveInput != Vector2.zero)
+        {
+            playerMoving = true;
+            rigidBody.velocity = new Vector2(moveInput.x * moveSpeed, moveInput.y * moveSpeed);
+            lastMove = moveInput;
+        } else {
+            rigidBody.velocity = Vector2.zero;
+        }
+
+        animator.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        animator.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        animator.SetBool("PlayerMoving", playerMoving);
+        animator.SetFloat("LastMoveX", lastMove.x);
+        animator.SetFloat("LastMoveY", lastMove.y);
+    }
+}
