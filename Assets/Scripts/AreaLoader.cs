844f4e081de50fda7981d1f9162c7d49d19c483c
unknown <jrffmatias@gmail.com>
2021-02-21 12:04:22 +0000

    Add functional scene switch
    Player is now able to enter building B

---
+++ Assets/Scripts/AreaLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AreaLoader : MonoBehaviour
+{
+    public string levelToLoad;
+    public string exitPoint;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.name == "Player"){
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+}
