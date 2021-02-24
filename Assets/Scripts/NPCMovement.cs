08d88550bbb5e291b0b0e60d52dfff7fceb9b06b
CMGS5 <37114874+CMGS5@users.noreply.github.com>
2021-02-24 15:56:02 +0000

    npc moving

---
+++ Assets/Scripts/NPCMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCMovement : MonoBehaviour
+
+{
+    public float moveSpeed;
+
+    private Rigidbody2D rigidBody;
+    private Animator animator;
+
+    private float moveCounter;
+
+    private bool moveRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        moveCounter = 0;
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveCounter += Time.deltaTime;
+
+        if (moveCounter < 4.5 || moveCounter > 6 && moveCounter < 30) {
+            moveRight = true;
+            rigidBody.velocity = new Vector2(moveSpeed, 0);
+        }
+
+        else if(moveCounter > 4.5 && moveCounter < 6 )
+        {
+            moveRight = false;
+            rigidBody.velocity = new Vector2(0, moveSpeed);
+        }
+
+        else rigidBody.velocity = Vector2.zero;
+
+        animator.SetBool("MoveRight", moveRight);
+    }
+}
