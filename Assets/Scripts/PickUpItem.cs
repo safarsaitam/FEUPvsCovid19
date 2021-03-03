b91cbccc2b023015c1cc1078a5e37cec85994e03
CMGS5 <37114874+CMGS5@users.noreply.github.com>
2021-03-03 15:00:14 +0000

    pick up item

---
+++ Assets/Scripts/PickUpItem.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpItem : MonoBehaviour
+
+{
+    private bool inRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(inRange && Input.GetKeyDown(KeyCode.E))
+        {
+            PickUp();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            inRange = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            inRange = false;
+        }
+    }
+
+    void PickUp()
+    {
+        gameObject.SetActive(false);
+    }
+
+
+}
